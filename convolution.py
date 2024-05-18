feat and fix: create main and subroutine scripts: convolution.py, dft_product.py, main.py, normalization.py and show_fft.py & use np.pad instead of resize_kernel.py

convolution.py
@@ -24,51 +24,36 @@ SOFTWARE.
 
 import numpy as np
 import cv2
-from dft_product import dft_product
-from convolution import convolution
+from scipy.signal import convolve2d
+from normalization import normalization
 
-def main():
+def convolution(img, kernel):
+    conv_result = convolve2d(img, kernel, 'same')
+    conv_result = normalization(conv_result)
+    conv_result = np.uint8(conv_result)
+
+    return conv_result
+
+
+if __name__ == "__main__":
     img = cv2.imread("./photos/profile_photo_501.jpg", 0)
 
-    # High-pass kernel
+    # Input High-pass kernel
     high_pass_kernel = np.array([[1, 1, 1],
                                  [1, -8, 1],
                                  [1, 1, 1]], dtype='int8')
-    # Low-pass kernel
-    kernel_size = 15
-    low_pass_kernel = np.ones((kernel_size, kernel_size), np.float64) / kernel_size ** 2
-
-    # Input High-pass kernel to dft_product
-    high_pass_result = dft_product(img, high_pass_kernel, "high_pass")
-    high_pass_result = np.where(high_pass_result > 160, high_pass_result, 0)  # 邊緣 Threshold 設定 160
-    cv2.namedWindow("high_pass_result", cv2.WINDOW_NORMAL)
-    cv2.resizeWindow("high_pass_result", 500, 500)
-    cv2.imshow("high_pass_result", high_pass_result)
-
-    # Input Low-pass kernel to dft_product
-    low_pass_result = dft_product(img, low_pass_kernel, "low_pass")
-    cv2.namedWindow("low_pass_result", cv2.WINDOW_NORMAL)
-    cv2.resizeWindow("low_pass_result", 500, 500)
-    cv2.imshow("low_pass_result", low_pass_result)
-
-    # Input High-pass kernel to convolution
     high_pass_conv_result = convolution(img, high_pass_kernel)
     high_pass_conv_result = np.where(high_pass_conv_result > 160, high_pass_conv_result, 0)
     cv2.namedWindow("high_pass_conv_result", cv2.WINDOW_NORMAL)
     cv2.resizeWindow("high_pass_conv_result", 500, 500)
     cv2.imshow("high_pass_conv_result", high_pass_conv_result)
 
-    # Input Low-pass kernel to convolution
+    # Input Low-pass kernel
+    kernel_size = 15
+    low_pass_kernel = np.ones((kernel_size, kernel_size), np.float64) / kernel_size ** 2
     low_pass_conv_result = convolution(img, low_pass_kernel)
     cv2.namedWindow("low_pass_conv_result", cv2.WINDOW_NORMAL)
     cv2.resizeWindow("low_pass_conv_result", 500, 500)
     cv2.imshow("low_pass_conv_result", low_pass_conv_result)
 
     cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-    return 0
-
-
-if __name__ == "__main__":
-    main()

dft_product.py
@@ -24,51 +24,60 @@ SOFTWARE.
 
 import numpy as np
 import cv2
-from dft_product import dft_product
-from convolution import convolution
+from show_fft import show_fft
+from normalization import normalization
 
-def main():
+
+def dft_product(img, kernel, filter_type):
+    def pad_with(vector, pad_width, iaxis, kwargs):
+        pad_value = kwargs.get('padder', 10)
+        vector[:pad_width[0]] = pad_value
+        vector[-pad_width[1]:] = pad_value
+
+    img = np.array(img, dtype='uint8')
+
+    kernel_resized = np.pad(kernel, int((img.shape[0]-kernel.shape[0])/2), pad_with, padder=0)
+
+    fft_img = np.fft.fft2(img)
+    fft_shift_img = np.fft.fftshift(fft_img)
+    show_fft(fft_shift_img, filter_type+"_fft_shift_img")
+
+    fft_kernel = np.fft.fft2(kernel_resized)
+    fft_shift_kernel = np.fft.fftshift(fft_kernel)
+    show_fft(fft_shift_kernel, filter_type+"_fft_shift_kernel")
+
+    fft_product_result = fft_shift_img * fft_shift_kernel
+    show_fft(fft_product_result, filter_type+"_fft_product_result")
+
+    ifftshift_product_result = np.fft.ifftshift(fft_product_result)
+    ifft_product_result = np.fft.ifft2(ifftshift_product_result)
+    ifft_product_result = np.real(ifft_product_result)
+    ifft_product_result = normalization(ifft_product_result)
+    ifft_product_result = np.uint8(ifft_product_result)
+    final_result = np.fft.fftshift(ifft_product_result)   # 矯正奇怪的錯位
+
+    return final_result
+
+
+if __name__ == "__main__":
     img = cv2.imread("./photos/profile_photo_501.jpg", 0)
 
-    # High-pass kernel
+    # Input High-pass kernel
     high_pass_kernel = np.array([[1, 1, 1],
                                  [1, -8, 1],
                                  [1, 1, 1]], dtype='int8')
-    # Low-pass kernel
-    kernel_size = 15
-    low_pass_kernel = np.ones((kernel_size, kernel_size), np.float64) / kernel_size ** 2
-
-    # Input High-pass kernel to dft_product
     high_pass_result = dft_product(img, high_pass_kernel, "high_pass")
-    high_pass_result = np.where(high_pass_result > 160, high_pass_result, 0)  # 邊緣 Threshold 設定 160
+    high_pass_result = np.where(high_pass_result > 160, high_pass_result, 0)   # 邊緣 Threshold 設定 160
     cv2.namedWindow("high_pass_result", cv2.WINDOW_NORMAL)
     cv2.resizeWindow("high_pass_result", 500, 500)
     cv2.imshow("high_pass_result", high_pass_result)
 
-    # Input Low-pass kernel to dft_product
+    # Input Low-pass kernel
+    kernel_size = 15
+    low_pass_kernel = np.ones((kernel_size, kernel_size), np.float64) / kernel_size ** 2
     low_pass_result = dft_product(img, low_pass_kernel, "low_pass")
     cv2.namedWindow("low_pass_result", cv2.WINDOW_NORMAL)
     cv2.resizeWindow("low_pass_result", 500, 500)
     cv2.imshow("low_pass_result", low_pass_result)
 
-    # Input High-pass kernel to convolution
-    high_pass_conv_result = convolution(img, high_pass_kernel)
-    high_pass_conv_result = np.where(high_pass_conv_result > 160, high_pass_conv_result, 0)
-    cv2.namedWindow("high_pass_conv_result", cv2.WINDOW_NORMAL)
-    cv2.resizeWindow("high_pass_conv_result", 500, 500)
-    cv2.imshow("high_pass_conv_result", high_pass_conv_result)
-
-    # Input Low-pass kernel to convolution
-    low_pass_conv_result = convolution(img, low_pass_kernel)
-    cv2.namedWindow("low_pass_conv_result", cv2.WINDOW_NORMAL)
-    cv2.resizeWindow("low_pass_conv_result", 500, 500)
-    cv2.imshow("low_pass_conv_result", low_pass_conv_result)
-
     cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-    return 0
-
-
-if __name__ == "__main__":
-    main()

fft_ifft_test.py
@@ -26,7 +26,7 @@ import numpy as np
 import cv2
 from resize_kernel import resize_kernel
 
-img = cv2.imread("./photos/profile_photo.jpg", 0)
+img = cv2.imread("./photos/profile_photo_255.jpg", 0)
 
 # kernel = np.array([[ 0,-1, 0],
 #                              [-1, 4,-1],

normalization.py
@@ -23,4 +23,12 @@ SOFTWARE.
 """
 
 import numpy as np
-import cv2
+
+def normalization(arr):
+    min_v = np.min(arr)
+    max_v = np.max(arr)
+
+    arr = (arr - min_v) / (max_v - min_v)
+    arr = (arr * 255).astype(np.uint8)
+
+    return arr

show_fft.py
@@ -26,10 +26,10 @@ import numpy as np
 import cv2
 
 def show_fft(fft, name):
-    fft = 20 * np.log(np.abs(fft) + 1)
+    fft = np.log(np.abs(fft) + 1)
     fft = fft / fft.max() * 255.0
     fft = np.uint8(fft)
 
     cv2.namedWindow(name, cv2.WINDOW_NORMAL)
-    cv2.resizeWindow(name, 700, 700)
+    cv2.resizeWindow(name, 500, 500)
     cv2.imshow(name, fft)